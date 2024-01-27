4d610f22cd9f22b8add72e35025b030c0e699c68
Sergey Davydov <dev@sdcollection.com>
2024-01-27 17:17:05 +0300

    tune log

--- internal/app/handler/routes.go
+++ internal/app/handler/routes.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"errors"
+	myErr "github.com/MrSwed/go-musthave-shortener/internal/app/errors"
 	"github.com/gin-gonic/gin"
 	"log"
 	"net/http"
@@ -22,7 +24,7 @@ func (h *Handler) MakeShort() func(c *gin.Context) {
 		html, err := h.s.NewShort(string(url))
 		if err != nil {
 			c.AbortWithStatus(http.StatusInternalServerError)
-			log.Printf("Error reate new short %s", err)
+			log.Printf("Error create new short %s", err)
 		}
 		c.Header("Content-Type", "text/plain; charset=utf-8")
 		c.String(http.StatusCreated, html)
@@ -33,7 +35,12 @@ func (h *Handler) GetShort() func(c *gin.Context) {
 	return func(c *gin.Context) {
 		id := c.Param("id")
 		if newURL, err := h.s.GetFromShort(id); err != nil {
-			c.AbortWithStatus(http.StatusBadRequest)
+			if errors.Is(err, myErr.ErrNotExist) {
+				c.AbortWithStatus(http.StatusBadRequest)
+			} else {
+				c.AbortWithStatus(http.StatusInternalServerError)
+				log.Printf("Error get new short %s", err)
+			}
 			return
 		} else {
 			c.Redirect(http.StatusTemporaryRedirect, newURL)

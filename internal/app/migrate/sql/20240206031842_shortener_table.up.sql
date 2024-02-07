336908ed4d3fd638c1bc438bdd8adfc260b07644
Sergey Davydov <dev@sdcollection.com>
2024-02-07 13:09:30 +0300

    correct migration

--- internal/app/migrate/sql/20240206031842_shortener_table.up.sql
+++ internal/app/migrate/sql/20240206031842_shortener_table.up.sql
@@ -3,7 +3,9 @@ create table shortener
  uuid  uuid default gen_random_uuid() not null
   constraint shortener_pk
    primary key,
- short varchar(8)                     not null,
+ short varchar(8)                     not null
+  constraint shortener_short
+   unique,
  url   varchar(2048)                  not null
-);
+)
 
